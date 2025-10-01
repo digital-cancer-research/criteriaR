c9ea128dafb5690d99e45f042cc5f8ae65a53d47
Paul O'Regan <paul.oregan@digitalecmt.org>
2025-10-01 16:41:27 +0100

    add headers, update DESCRIPTION

--- R/checkPrompt1Outputs.R
+++ R/checkPrompt1Outputs.R
@@ -1,3 +1,11 @@
+# Copyright 2025 Cancer Research Technology Limited.
+#
+# Licensed under a software academic use license provided with this software package (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at [link to package location of the license]
+# For commercial use, please contact Cancer Research Horizons at crh-cpmarketing@cancer.org.uk
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
 #' Check whether first prompts have already been used
 #'
 #' If the pipeline has been run before, check for each trial whether an identical prompt1 has been sent to LLM before.\cr
